a674f02a646494ad2ee17e08f7348387cc6f9fc0
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-01 16:12:38 +0200

    Exercise concluded.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,43 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
-const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
+// const modal = document.getElementById("myModal");
+// let promiseOfModal = new Promise(function (resolve) {
+//   window.setTimeout(function () {
+//     resolve(modal);
+//   }, 1000);
+// });
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+// promiseOfModal.then(function (val) {
+//   console.log("User has been on the page for 60 seconds");
+//   val.style.display = "block";
+// });
 
-modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
+// modal.addEventListener("click", (e) => {
+//   switch (e.target.className) {
+//     case "close":
+//     case "modal":
+//       modal.style.display = "none";
+//       break;
+//   }
+// });
+
+// Async / Await syntax:
+
+(async function openModal() {
+  const modal = document.getElementById("myModal");
+  const modalTimer = (ms = 1000 * 60) => new Promise((t) => setTimeout(t, ms));
+
+  await modalTimer();
+
+  modal.style.display = "block";
+
+  modal.addEventListener("click", (e) => {
+    switch (e.target.className) {
+      case "close":
+      case "modal":
+        modal.style.display = "none";
         break;
     }
-})
-
+  });
+})();
